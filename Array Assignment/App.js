3f22fd6cc72cb09fca4fe8ead5670da19ea9a64a
Shahmir Zaman <shahmirzaman009@gmail.com>
2023-08-19 22:22:58 +0500

    array assignment updated

--- Array Assignment/App.js
+++ Array Assignment/App.js
@@ -1,4 +1,6 @@
-let fruits = ["Mango","Banana","Orange","Apple","Pineapple","Grape",12];
+let fruits = ["Mango","Banana","Orange","Apple","Pineapple"];
+fruits[fruits.length] = 12;
+// console.log(fruits);
 let firstFruit = fruits[0];
 fruits[fruits.length-1] = "Strawberry";
 console.log(firstFruit);
