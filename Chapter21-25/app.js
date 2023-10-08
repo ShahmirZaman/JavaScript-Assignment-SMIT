65d39db1490604ec14fddfae63af37cb4b854093
Shahmir Zaman <shahmirzaman009@gmail.com>
2023-10-08 18:07:35 +0500

    chapter21-25 in progress

---
+++ Chapter21-25/app.js
@@ -0,0 +1,39 @@
+//Question1 
+// document.write("<h3>Question1</h3>");
+// let firstName = prompt("Enter your first name: ");
+// let lastName = prompt("Enter your last name: ");
+// let fullName = firstName + " " + lastName;
+// document.write(`Hello ${fullName} <hr>`);
+
+//Question2
+// document.write("<h3>Question2</h3>");
+// let favPhone = prompt("Enter your favourite mobile phone model: ");
+// document.write(`My favourite phone is: ${favPhone} <br>`);
+// document.write(`Length of string: ${favPhone.length} <hr>`);
+
+//Question3
+// document.write("<h3>Question3</h3>");
+// let word = "Pakistani";
+// let indexWord = word.indexOf("n");
+// document.write(`String: ${word} <br> Index of 'n': ${indexWord} <hr>`);
+
+//Question4
+// document.write("<h3>Question4</h3>");
+// let word1 = "Hello World";
+// let indexWord1 = word1.lastIndexOf("l")
+// document.write(`String: ${word1} <br> Index of 'l': ${indexWord1} <hr>`);
+
+//Question5
+// document.write("<h3>Question5</h3>");
+// let word2 = "Pakistani";
+// let charAtWord = word2.charAt(3);
+// document.write(`String: ${word2} <br> Character at index 3: ${charAtWord} <hr>`);
+
+//Question6
+document.write("<h3>Question6</h3>");
+let firstName1 = prompt("Enter your first name: ");
+let lastName1 = prompt("Enter your last name: ");
+firstName1 = firstName1 + " ";
+let fullName1 = firstName1.concat(lastName1);
+document.write(`Hello ${fullName1} <hr>`);
+
